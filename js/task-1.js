50bed1d474b347ad69948692219fd979818d0dad
Ivanka Haidai <ivankia01@ukr.net>
2025-04-19 09:27:26 +0300

    add tasks 1,2

--- js/task-1.js
+++ js/task-1.js
@@ -1,30 +1,11 @@
-const customer = {
- username: "Mango",
- balance: 24000,
- discount: 0.1,
- orders: ["Burger", "Pizza", "Salad"],
- // Change code below this line
- getBalance() {
-   return this.balance;
- },
- getDiscount() {
-   return this.discount;
- },
- setDiscount(value) {
-   this.discount = value;
- },
- getOrders() {
-   return this.orders;
- },
- addOrder(cost, order) {
-   this.balance -= cost - cost * this.discount;
-   this.orders.push(order);
- },
- // Change code above this line
-};
+const ulElem = document.querySelector("ul#categories");
+const liElems = ulElem.childElementCount;
+console.log(`Numbers of categories: ${liElems}`);
 
-customer.setDiscount(0.15);
-console.log(customer.getDiscount()); // 0.15
-customer.addOrder(5000, "Steak");
-console.log(customer.getBalance()); // 19750
-console.log(customer.getOrders()); // ["Burger", "Pizza", "Salad", "Steak"]
+const liElemsAll = document.querySelectorAll(".item")
+liElemsAll.forEach((elem) => {
+  console.log("Category: ", elem.firstElementChild.textContent);
+  console.log("Elements: ", elem.lastElementChild.children.length);
+})
+
+
